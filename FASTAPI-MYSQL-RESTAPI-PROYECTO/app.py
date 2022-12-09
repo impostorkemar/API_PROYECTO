8f5a1e271e80b34e79c7a763148b267af7c7c21f
Jefferson Cajas <38546533+impostorkemar@users.noreply.github.com>
2022-12-09 06:21:09 -0500

    Update app.py

--- FASTAPI-MYSQL-RESTAPI-PROYECTO/app.py
+++ FASTAPI-MYSQL-RESTAPI-PROYECTO/app.py
@@ -7,6 +7,26 @@ app = FastAPI()
 origins =[
     "http://localhost:8000",
     "http://localhost:8001",
+    "https://127.0.0.1:8000",
+    "https://127.0.0.1:8001",
+    "http://localhost:4200",
+    "https://localhost:4200",
+    "http://192.168.1.11:4200",
+    "https://192.168.1.11:4200",
+    "http://192.168.1.35:8000",
+    "https://192.168.1.35:8000",
+    "http://192.168.1.35:4200",
+    "https://192.168.1.35:4200",
+    "http://192.168.100.81:4200",
+    "https://192.168.1.81:4200",
+    "https://main.d12wuq988la98b.amplifyapp.com",
+    "http://main.d12wuq988la98b.amplifyapp.com",
+    "https://main.d12wuq988la98b.amplifyapp.com:4200",
+    "http://main.d12wuq988la98b.amplifyapp.com:4200",
+    "http://54.197.20.174/",
+    "https://54.197.20.174/"
+
+    
 ]
 #adding middleware uris
 app.add_middleware(
