fd17b29aad76c94d815fe99c7764c8d88b8bcaa8
Jefferson Cajas <38546533+impostorkemar@users.noreply.github.com>
2022-12-09 06:15:22 -0500

    Update app.py

--- FASTAPI-MYSQL-RESTAPI-PROYECTO/app.py
+++ FASTAPI-MYSQL-RESTAPI-PROYECTO/app.py
@@ -4,36 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 #adding cors uris
-origins =[
-    "http://localhost:8000",
-    "http://localhost:8001",
-    "https://127.0.0.1:8000",
-    "https://127.0.0.1:8001",
-    "http://localhost:4200",
-    "https://localhost:4200",
-    "http://192.168.1.11:4200",
-    "https://192.168.1.11:4200",
-    "http://192.168.1.35:8000",
-    "https://192.168.1.35:8000",
-    "http://192.168.1.35:4200",
-    "https://192.168.1.35:4200",
-    "http://192.168.100.81:4200",
-    "https://192.168.1.81:4200",
-    "https://main.d12wuq988la98b.amplifyapp.com",
-    "http://main.d12wuq988la98b.amplifyapp.com",
-    "https://main.d12wuq988la98b.amplifyapp.com:4200",
-    "http://main.d12wuq988la98b.amplifyapp.com:4200"
 
-    
-]
-#adding middleware uris
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
 
 app.include_router(user)
 
