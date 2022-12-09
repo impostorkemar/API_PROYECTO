32b57e8db90f91d1cd673973644b9e15f7b9adb3
Jefferson Cajas <38546533+impostorkemar@users.noreply.github.com>
2022-12-09 05:43:41 -0500

    Update app.py

--- FASTAPI-MYSQL-RESTAPI-PROYECTO/app.py
+++ FASTAPI-MYSQL-RESTAPI-PROYECTO/app.py
@@ -30,7 +30,7 @@ origins =[
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=["*"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
